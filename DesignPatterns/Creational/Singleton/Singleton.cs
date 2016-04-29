8df0404aebd384c718a7036a6e86c509d9e0923b
Łukasz Krzyszczak <lukasz.krzyszczak@gmail.com>
2016-04-29 14:20:44 +0200

    Add Singleton pattern

---
+++ DesignPatterns/Creational/Singleton/Singleton.cs
@@ -0,0 +1,24 @@
+﻿namespace DesignPatterns.Creational.Singleton
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+
+                return instance;
+            }            
+        }
+    }
+}
---
+++ DesignPatterns/Creational/Singleton/ThreadSafeSingleton.cs
@@ -0,0 +1,22 @@
+﻿namespace DesignPatterns.Creational.Singleton
+{
+    using System;
+
+    public sealed class ThreadSafeSingleton
+    {
+        private static readonly Lazy<ThreadSafeSingleton> lazy =
+            new Lazy<ThreadSafeSingleton>(() => new ThreadSafeSingleton());
+
+        private ThreadSafeSingleton()
+        {
+        }
+
+        public static ThreadSafeSingleton Instance
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
+    }
+}
