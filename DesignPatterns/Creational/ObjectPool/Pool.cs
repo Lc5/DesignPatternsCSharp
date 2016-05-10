236dd8bb9b3e5d064a43fbc382dd154b01ae632b
Łukasz Krzyszczak <lukasz.krzyszczak@gmail.com>
2016-05-10 14:01:11 +0200

    Add Object Pool pattern

---
+++ DesignPatterns/Creational/ObjectPool/Pool.cs
@@ -0,0 +1,43 @@
+﻿namespace DesignPatterns.Creational.ObjectPool
+{
+    using System.Collections.Generic;
+
+    public static class Pool<T> where T : PooledObject, new()
+    {
+        private static List<T> available = new List<T>();
+
+        private static List<T> inUse = new List<T>();
+
+        public static T GetObject()
+        {
+            lock(available)
+            {
+                if (available.Count > 0)
+                {
+                    T obj = available[0];
+                    available.RemoveAt(0);
+                    inUse.Add(obj);
+
+                    return obj;
+                }
+                else
+                {
+                    T obj = new T();
+                    inUse.Add(obj);
+
+                    return obj;
+                }
+            }
+        }
+
+        public static void ReleaseObject(T obj)
+        {
+            lock (available)
+            {
+                obj.Cleanup();
+                inUse.Remove(obj);
+                available.Add(obj);
+            }
+        }
+    }
+}
