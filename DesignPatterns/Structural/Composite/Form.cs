162fa1f0d4e90ca89de5fc62566e74b2c64fae20
Łukasz Krzyszczak <lukasz.krzyszczak@gmail.com>
2016-05-12 09:43:59 +0200

    Add Composite pattern

---
+++ DesignPatterns/Structural/Composite/Form.cs
@@ -0,0 +1,32 @@
+﻿namespace DesignPatterns.Structural.Composite
+{
+    using System.Collections.Generic;
+
+    using System.Text;
+
+    public class Form : IFormElement
+    {
+        private List<IFormElement> elements = new List<IFormElement>();
+
+        public string Render(int indent = 0)
+        {
+            var output = new StringBuilder();
+
+            output.Append(new string(' ', indent) + "<form>\r\n");
+
+            foreach (var element in this.elements)
+            {
+                output.Append(element.Render(indent + 4));
+            }
+
+            output.Append(new string(' ', indent) + "</form>\r\n");
+
+            return output.ToString();
+        }
+
+        public void AddElement(IFormElement element)
+        {
+            this.elements.Add(element);
+        }
+    }
+}
---
+++ DesignPatterns/Structural/Composite/InputElement.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatterns.Structural.Composite
+{
+    public class InputElement : IFormElement
+    {
+        public string Render(int indent = 0)
+        {
+            return new string(' ', indent) + "<input type=\"text\" />\r\n";
+        }
+    }
+}
