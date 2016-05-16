e28fc5ac1a9a803a79497ca826c3c018891192c8
Łukasz Krzyszczak <lukasz.krzyszczak@gmail.com>
2016-05-16 17:35:01 +0200

    Add Interpreter pattern

---
+++ DesignPatterns/Behavioral/Interpreter/IExpression.cs
@@ -0,0 +1,7 @@
+﻿namespace DesignPatterns.Behavioral.Interpreter
+{
+    public interface IExpression
+    {
+        int Interpret();
+    }
+}
---
+++ DesignPatterns/Behavioral/Interpreter/NumberExpression.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPatterns.Behavioral.Interpreter
+{
+    public class NumberExpression : IExpression
+    {
+        private readonly int number;
+
+        public NumberExpression(int number)
+        {
+            this.number = number;
+        }
+
+        public int Interpret()
+        {
+            return this.number;
+        }
+    }
+}
